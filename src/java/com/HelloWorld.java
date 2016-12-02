d50d35a27a68c1f312d13a4413be7149c80b6afb
sreejith012 <sreejithpoothu@gmail.com>
2016-12-02 20:10:56 +0530

    Update HelloWorld.java

--- src/java/com/HelloWorld.java
+++ src/java/com/HelloWorld.java
@@ -23,7 +23,7 @@ public class HelloWorld extends HttpServlet
         String color = request.getParameter("color");
         System.out.println(color);
         PrintWriter out = response.getWriter();
-        out.println((new StringBuilder("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n<html> \n<head> \n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"> \n<title> My first jsp </title> \n</head> \n<body> \n<font size=\"12px\" color=\"")).append(color).append("\">").append("Hello").append("</font> \n").append("</body> \n").append("</html>").toString());
+        out.println((new StringBuilder("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n<html> \n<head> \n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"> \n<title> JSP demo </title> \n</head> \n<body> \n<font size=\"12px\" color=\"")).append(color).append("\">").append("Hello").append("</font> \n").append("</body> \n").append("</html>").toString());
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
